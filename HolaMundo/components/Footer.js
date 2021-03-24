45a8b34582284ee779ad10ee7b29accda601ce11
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-03-24 16:25:22 -0400

    Comunication between components, from index to components folder through props

--- HolaMundo/components/Footer.js
+++ HolaMundo/components/Footer.js
@@ -1,7 +1,14 @@
 app.component('footer-banco', {
+    props: ['cantidad', 'fecha'],
     template: /*html*/ `
     <div class="bg-dark py-3 mt-2 text-white">
-        <h3>Footer de mi sitio web</h3>
+        <h3> {{texto}} - {{cantidad}} </h3>
+        <p> {{fecha}}  </p>
     </div>
-    `
+    `,
+    data(){
+        return {
+            texto: 'Footer de mi sitio web'
+        }
+    }
 })
