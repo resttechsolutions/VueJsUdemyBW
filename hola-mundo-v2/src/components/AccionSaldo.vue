d7f9e5a01f7eb2f1b4bd5295acdb2e14976915cb
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-03-25 14:24:54 -0400

    Creating buttons in Cuenta with props tu use them later as add or rem saldo

---
+++ hola-mundo-v2/src/components/AccionSaldo.vue
@@ -0,0 +1,22 @@
+<template>
+  <button> {{texto}}  </button>
+</template>
+
+<script>
+export default {
+    name: 'AccionSaldo',
+    props:{
+        texto: String
+    },
+    data() {
+        return {
+            
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
--- hola-mundo-v2/src/components/Cuenta.vue
+++ hola-mundo-v2/src/components/Cuenta.vue
@@ -5,10 +5,17 @@
     <div v-for="(servicio, index) in servicios" :key="index">
         {{index + 1}} - {{ servicio }}
     </div>
+    <AccionSaldo texto="Aumentar Saldo"/>
+    <AccionSaldo texto="Disminuir Saldo"/>
 </template>
 
 <script>
+import AccionSaldo from './AccionSaldo'
+
 export default {
+    components: {
+        AccionSaldo
+    },
     name: 'Cuenta',
     data() {
         return {
@@ -16,6 +23,14 @@ export default {
             cuenta: 'Visa',
             estado: true,
             servicios: ['giro', 'abono', 'transferencia']
+        }
+    },
+    methods: {
+        aumentar(){
+            this.saldo += 100
+        },
+        disminuir(){
+            this.saldo -= 100
         }
     },
 
