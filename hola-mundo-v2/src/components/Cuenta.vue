ca9bf1169c1d09cd75b96b87534707908cbf3ee9
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-03-25 02:01:30 -0400

    Testing CLI, reviewing components, props, data, imports, etc...

---
+++ hola-mundo-v2/src/components/Cuenta.vue
@@ -0,0 +1,27 @@
+<template>
+    <h2>Tipo de cuenta {{cuenta}}</h2>
+    <h2>Saldo: {{saldo}} </h2>
+    <h2>Cuenta: {{estado ? 'Cuenta activa' : 'Cuenta desactivada'}} </h2>
+    <div v-for="(servicio, index) in servicios" :key="index">
+        {{index + 1}} - {{ servicio }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Cuenta',
+    data() {
+        return {
+            saldo: 1000,
+            cuenta: 'Visa',
+            estado: true,
+            servicios: ['giro', 'abono', 'transferencia']
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
