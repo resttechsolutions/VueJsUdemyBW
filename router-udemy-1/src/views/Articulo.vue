a706cb583e5edfdf47f3c96042664075e6ddd77d
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-03-26 09:25:17 -0400

    Router with params

---
+++ router-udemy-1/src/views/Articulo.vue
@@ -0,0 +1,17 @@
+<template>
+  <Titulo texto="Ruta con parametros"/>
+  <h2>Parametro: {{$route.params.id}} </h2>
+</template>
+
+<script>
+import Titulo from '../components/Titulo'
+export default {
+    components:{
+        Titulo
+    }
+}
+</script>
+
+<style>
+
+</style>
