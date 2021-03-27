619c5ce815272cc1479a6329c75e47dd7ca18873
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-03-26 20:42:54 -0400

    Using fetch and showing data into blog page

--- router-udemy-1/src/views/Blog.vue
+++ router-udemy-1/src/views/Blog.vue
@@ -1,5 +1,14 @@
 <template>
-  <Titulo texto="Titulo de mi Blog"/>
+  <div>
+      <Titulo texto="Titulo de mi Blog"/>
+  <button @click="consumirApi">Consumir API</button>
+
+  <ul>
+      <li v-for="(blog, index) in arrayBlog" :key="index">
+          {{blog.id}} - {{blog.title}}
+      </li>
+  </ul>
+  </div>
 </template>
 
 <script>
@@ -7,6 +16,25 @@ import Titulo from '../components/Titulo'
 export default {
     components:{
         Titulo
+    },
+    data() {
+        return {
+            arrayBlog: []
+        }
+    },
+    methods: {
+        async consumirApi(){
+            try {
+                const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+                const array = await data.json()
+
+                this.arrayBlog = array
+
+                console.log(array)
+            } catch (error) {
+                console.log(error)
+            }
+        }
     }
 }
 </script>
