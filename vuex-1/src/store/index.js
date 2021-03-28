b5c099c8c3608b326fff2a14a051f47cdb5ffa8d
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-03-28 18:46:58 -0400

    Using vuex with mapState

---
+++ vuex-1/src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    contador: 100
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
---
+++ vuex-1/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Mi contador: {{contador}} </h1>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+
+export default {
+  name: 'Home',
+  components: {
+    
+  },
+  computed:{
+    ...mapState(['contador'])
+  }
+}
+</script>
