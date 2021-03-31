e7ed6633688424d5cdff1ff6133f8d341b3cdac8
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-03-31 01:55:01 -0400

    LocaleStorage

--- formulario-crud/src/App.vue
+++ formulario-crud/src/App.vue
@@ -6,11 +6,19 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex'
+
 import Navbar from './components/Navbar';
 
 export default {
   components: {
     Navbar
-  }
+  },
+  methods:{
+    ...mapActions(['cargarLocalStorage'])
+  },
+  created() {
+    this.cargarLocalStorage()
+  },
 }
 </script>
--- formulario-crud/src/store/index.js
+++ formulario-crud/src/store/index.js
@@ -3,48 +3,62 @@ import router from '../router'
 
 export default createStore({
   state: {
-    tareas:[],
-    tarea:{
+    tareas: [],
+    tarea: {
       id: '',
-      nombre:'',
+      nombre: '',
       categorias: [],
-      estado:'',
+      estado: '',
       numero: 0
     }
   },
   mutations: {
-    set(state, payload){
+    cargar(state, payload){
+      state.tareas = payload
+    },
+    set(state, payload) {
       state.tareas.push(payload)
-      console.log(state.tareas)
+      localStorage.setItem('tareas', JSON.stringify(state.tareas))
     },
-    eliminar(state, payload){
+    eliminar(state, payload) {
       state.tareas = state.tareas.filter(item => item.id !== payload)
+      localStorage.setItem('tareas', JSON.stringify(state.tareas))
     },
-    tarea(state, payload){
-      if(! (state.tareas = state.tareas.find(item => item.id === payload))){
+    tarea(state, payload) {
+      if (!state.tareas.find(item => item.id === payload)) {
         router.push('/')
         return
       }
 
-      state.tareas = state.tareas.find(item => item.id === payload)
-      
+      state.tarea = state.tareas.find(item => item.id === payload)
+
     },
-    update(state, payload){
+    update(state, payload) {
       state.tareas = state.tareas.map(item => item.id === payload.id ? payload : item)
       router.push('/')
+      localStorage.setItem('tareas', JSON.stringify(state.tareas))
     }
   },
   actions: {
-    setTareas({commit}, tarea){
+    cargarLocalStorage({ commit }) {
+      if (localStorage.getItem('tareas')) { 
+        const tareas = JSON.parse(localStorage.getItem('tareas'))
+        commit('cargar', tareas) 
+        return
+      }
+
+      localStorage.setItem('tareas', JSON.stringify([]))
+    },
+    setTareas({ commit }, tarea) {
       commit('set', tarea)
     },
-    deleteTareas({commit}, id){
+    deleteTareas({ commit }, id) {
       commit('eliminar', id)
     },
-    setTarea({commit}, id){
+    setTarea({ commit }, id) {
       commit('tarea', id)
     },
-    updateTarea({commit}, tarea){
+    updateTarea({ commit }, tarea) {
       commit('update', tarea)
     }
   },
