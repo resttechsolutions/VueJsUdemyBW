1a91f1dc9b7a3e13b0cbaf29bf3598da595a46e0
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-03-31 01:28:25 -0400

    Updating task and routing

--- formulario-crud/src/store/index.js
+++ formulario-crud/src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import router from '../router'
 
 export default createStore({
   state: {
@@ -20,7 +21,17 @@ export default createStore({
       state.tareas = state.tareas.filter(item => item.id !== payload)
     },
     tarea(state, payload){
-      state.tarea = state.tareas.find(item => item.id === payload)
+      if(! (state.tareas = state.tareas.find(item => item.id === payload))){
+        router.push('/')
+        return
+      }
+
+      state.tareas = state.tareas.find(item => item.id === payload)
+      
+    },
+    update(state, payload){
+      state.tareas = state.tareas.map(item => item.id === payload.id ? payload : item)
+      router.push('/')
     }
   },
   actions: {
@@ -32,6 +43,9 @@ export default createStore({
     },
     setTarea({commit}, id){
       commit('tarea', id)
+    },
+    updateTarea({commit}, tarea){
+      commit('update', tarea)
     }
   },
   modules: {
