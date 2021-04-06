b7f4924b7aef8c8fa74e6273c21efe9aad5e6f16
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-04-05 23:22:01 -0400

    Creatint and Reading with Firebase

---
+++ api-firebase/src/components/ListaTareas.vue
@@ -0,0 +1,55 @@
+<template>
+  <!-- Listing Tasks -->
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Categorias</th>
+        <th scope="col">Estado</th>
+        <th scope="col">Numero</th>
+        <th scope="col">Accion</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in tareas" :key="item.id">
+        <th scope="row">{{ item.id }}</th>
+        <td>{{ item.nombre }}</td>
+        <td>
+          <!-- Listing Categories -->
+          {{ item.categorias.join(", ") }}
+        </td>
+        <td>{{ item.estado }}</td>
+        <td>{{ item.numero }}</td>
+        <td>
+          <!-- Delete Action -->
+          <button
+            class="btn btn-outline-danger btn-sm"
+            @click="deleteTareas(item.id)"
+          >
+            Eliminar
+          </button>
+          <router-link class="btn btn-outline-warning ml-2 btn-sm" :to="{
+              name:'Editar',
+              params: {
+                  id: item.id
+              }
+          }">Editar</router-link>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["tareas"]),
+  },
+  methods: {
+    ...mapActions(["deleteTareas"]),
+  },
+};
+</script>
---
+++ api-firebase/src/store/index.js
@@ -0,0 +1,84 @@
+import { createStore } from 'vuex'
+import router from '../router'
+
+export default createStore({
+  state: {
+    tareas: [],
+    tarea: {
+      id: '',
+      nombre: '',
+      categorias: [],
+      estado: '',
+      numero: 0
+    }
+  },
+  mutations: {
+    cargar(state, payload){
+      state.tareas = payload
+    },
+    set(state, payload) {
+      state.tareas.push(payload)
+      // localStorage.setItem('tareas', JSON.stringify(state.tareas))
+    },
+    eliminar(state, payload) {
+      state.tareas = state.tareas.filter(item => item.id !== payload)
+      // localStorage.setItem('tareas', JSON.stringify(state.tareas))
+    },
+    tarea(state, payload) {
+      if (!state.tareas.find(item => item.id === payload)) {
+        router.push('/')
+        return
+      }
+
+      state.tarea = state.tareas.find(item => item.id === payload)
+
+    },
+    update(state, payload) {
+      state.tareas = state.tareas.map(item => item.id === payload.id ? payload : item)
+      router.push('/')
+      // localStorage.setItem('tareas', JSON.stringify(state.tareas))
+    }
+  },
+  actions: {
+    async cargarLocalStorage({ commit }) {
+      const res = await fetch('https://udemy-api-1a8a8-default-rtdb.firebaseio.com/tareas.json')
+      const dataDB = await res.json()
+
+      const arrayTareas = []
+
+      for(let id in dataDB){
+        arrayTareas.push(dataDB[id])
+      }
+      commit('cargar', arrayTareas)
+    },
+    async setTareas({ commit }, tarea) {
+      try {
+        const res = await fetch(`https://udemy-api-1a8a8-default-rtdb.firebaseio.com/tareas/${tarea.id}.json`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type':'application/json'
+          },
+          body: JSON.stringify(tarea)
+        })
+
+        const dataDB = await res.json()
+
+        console.log(dataDB)
+      } catch (error) {
+        console.log(error)
+      }
+      commit('set', tarea)
+    },
+    deleteTareas({ commit }, id) {
+      commit('eliminar', id)
+    },
+    setTarea({ commit }, id) {
+      commit('tarea', id)
+    },
+    updateTarea({ commit }, tarea) {
+      commit('update', tarea)
+    }
+  },
+  modules: {
+  }
+})
